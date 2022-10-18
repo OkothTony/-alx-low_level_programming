09f596737f128958ea088a6b129ec63697d80de4
Norris <bedzon@ieee.org>
2022-10-18 17:44:25 -0400

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // JS Script
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
+const request = require('request');
+
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
+  console.log(JSON.parse(body).title);
 });
